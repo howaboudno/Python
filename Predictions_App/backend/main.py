881482f12dd85024df83f58448db7b0c5b93fdd6
Aboud Shamaan <aboud.shamaan@gmail.com>
2026-06-17 00:35:46 +0200

    routers updated in main.py

--- Predictions_App/backend/main.py
+++ Predictions_App/backend/main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI
-from backend.routers.auth import register, login
-from backend.routers.tournament import GET as tournament_GET, POST as tournament_POST
-from backend.routers.predictions import GET as predictions_GET, POST as predictions_POST
-from backend.routers.scoreboard import GET as scoreboard_GET
+from routers.auth import register, login
+from routers.tournament import GET as tournament_GET, POST as tournament_POST
+from routers.predictions import GET as predictions_GET, POST as predictions_POST
+from routers.scoreboard import GET as scoreboard_GET
+from routers.auth import change_password
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routers.auth import change_password
 
 app = FastAPI()
 
