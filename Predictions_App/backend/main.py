917d94e36a8ce818c3289f19b2f8df6b5bb29605
Aboud Shamaan <aboud.shamaan@gmail.com>
2026-06-17 00:33:08 +0200

    deploy_1

--- Predictions_App/backend/main.py
+++ Predictions_App/backend/main.py
@@ -10,7 +10,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "null"],
+    allow_origins=[
+        "http://localhost:5173",
+        "null",
+        "https://python-production-c646.up.railway.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
